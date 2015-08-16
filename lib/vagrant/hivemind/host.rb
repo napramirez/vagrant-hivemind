d36c4b0ec9bd17df102ec011b093590ce8e20342
Nap Ramirez <napramirez@gmail.com>
2015-08-16 14:19:14 -0400

    Used hash keys to the BOX_SIZES and BOX_TYPES for portability

--- lib/vagrant/hivemind/host.rb
+++ lib/vagrant/hivemind/host.rb
@@ -6,7 +6,7 @@ module Vagrant
       attr_accessor :hostname, :ip_address, :is_control, :memory_in_mb, :box, :is_gui
       @control = nil
 
-      def initialize(hostname, ip_address, is_control = false, memory_in_mb = Constants::BOX_SIZES[:small], box = Constants::BOX_TYPES[:server], is_gui = false)
+      def initialize(hostname, ip_address, is_control = false, memory_in_mb = :small, box = :server, is_gui = false)
         @hostname = hostname
         @ip_address = ip_address
         @is_control = is_control
@@ -17,7 +17,7 @@ module Vagrant
 
       def self.control
         unless @control
-          @control = self.new Constants::CONTROL, Util::Network.starting_ip_address, true, Constants::BOX_SIZES[:extra_small], Constants::BOX_TYPES[:server], false
+          @control = self.new Constants::CONTROL, Util::Network.starting_ip_address, true, :extra_small, :server, false
         end
         @control
       end
