8244f48f8f3b09047e112490d920e92d66721ff1
Nap Ramirez <napramirez@gmail.com>
2015-08-16 01:58:41 -0400

    Implemented init command framework

--- lib/vagrant/hivemind/commands/init.rb
+++ lib/vagrant/hivemind/commands/init.rb
@@ -1,4 +1,7 @@
-require 'vagrant/hivemind/constants'
+require "optparse"
+
+require "vagrant/hivemind/constants"
+require "vagrant/hivemind/util"
 
 module Vagrant
   module Hivemind
@@ -9,7 +12,38 @@ module Vagrant
         end
 
         def execute
-          puts "Init!"
+          options = {
+            :force     => false,
+            :directory => []
+          }
+
+          OptionParser.new do |o|
+            o.banner = "Usage: vagrant hivemind init [options]"
+            o.separator ""
+            o.separator "Options:"
+            o.separator ""
+
+            o.on("-f", "--force", "Overwrite an existing '#{Vagrant::Hivemind::Constants::HIVE_FILE}' with a new one") do |f|
+              options[:force] = f
+            end
+
+            o.on("-d", "--directory DIRECTORY", "Specify the directory where '#{Vagrant::Hivemind::Constants::HIVE_FILE}' will be created (default: current directory)") do |d|
+              options[:directory] << d
+            end
+          end.parse!
+
+          work_dir = options[:directory].empty? ? "." : options[:directory].first
+
+          if Vagrant::Hivemind::Util::HiveFile.exist? work_dir
+            if options[:force]
+              puts "TODO: Overwrite the Hive file with a new one"
+            else
+              puts "TODO: Warn that the Hive file already exists!"
+            end
+          else
+            puts "TODO: Create new Hive file"
+          end
+
           0
         end
       end
