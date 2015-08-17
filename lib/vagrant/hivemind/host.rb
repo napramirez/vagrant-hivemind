457cca8921dcd3a3272cfb28e784c5bd336b5b95
Nap Ramirez <napramirez@gmail.com>
2015-08-17 14:29:06 -0400

    Refactored the optional constructor arguments into options param

--- lib/vagrant/hivemind/host.rb
+++ lib/vagrant/hivemind/host.rb
@@ -6,17 +6,17 @@ module Vagrant
       attr_accessor :hostname, :ip_address, :is_control, :memory_in_mb, :box
       @control = nil
 
-      def initialize(hostname, ip_address, is_control = false, memory_in_mb = :small, box = :server)
-        @hostname = hostname
-        @ip_address = ip_address
-        @is_control = is_control
-        @memory_in_mb = memory_in_mb
-        @box = box
+      def initialize(hostname, ip_address, options = {})
+        @hostname     = hostname
+        @ip_address   = ip_address
+        @is_control   = options[:is_control]   || false
+        @memory_in_mb = options[:memory_in_mb] || :small
+        @box          = options[:box]          || :server
       end
 
       def self.control
         unless @control
-          @control = self.new Constants::CONTROL, Util::Network.starting_ip_address, true, :extra_small, :server
+          @control = self.new Constants::CONTROL, Util::Network.starting_ip_address, { is_control: true, memory_in_mb: :extra_small, box: :server }
         end
         @control
       end
