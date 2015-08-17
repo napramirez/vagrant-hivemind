4974b853d97e436d0502236a56691e4481fc7dc1
Nap Ramirez <napramirez@gmail.com>
2015-08-17 15:19:07 -0400

    Refactored the Host to comply to the changes to the Constants, Changed the values of box_size and box_type to string instead of symbol

--- lib/vagrant/hivemind/host.rb
+++ lib/vagrant/hivemind/host.rb
@@ -3,20 +3,27 @@ require_relative "constants"
 module Vagrant
   module Hivemind
     class Host
-      attr_accessor :hostname, :ip_address, :is_control, :memory_in_mb, :box
+      attr_accessor :hostname, :ip_address, :is_control, :box_size, :box_type
       @control = nil
 
       def initialize(hostname, ip_address, options = {})
-        @hostname     = hostname
-        @ip_address   = ip_address
-        @is_control   = options[:is_control]   || false
-        @memory_in_mb = options[:memory_in_mb] || :small
-        @box          = options[:box]          || :server
+        @hostname   = hostname
+        @ip_address = ip_address
+        @is_control = options[:is_control] || false
+        @box_size   = options[:box_size]   || :small.to_s
+        @box_type   = options[:box_type]   || :server.to_s
       end
 
       def self.control
         unless @control
-          @control = self.new Constants::CONTROL, Util::Network.starting_ip_address, { is_control: true, memory_in_mb: :extra_small, box: :server }
+          @control = self.new(
+            Constants::CONTROL_HOSTNAME,
+            Util::Network.starting_ip_address,
+            {
+              is_control: true,
+              box_size:   :small.to_s,
+              box_type:   :server.to_s
+            })
         end
         @control
       end
