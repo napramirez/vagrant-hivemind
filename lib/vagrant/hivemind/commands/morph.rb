a5436db0fce9d5e385477ca4c4239456631adba6
Nap Ramirez <napramirez@gmail.com>
2015-08-20 06:27:52 -0400

    WIP morph input validations

--- lib/vagrant/hivemind/commands/morph.rb
+++ lib/vagrant/hivemind/commands/morph.rb
@@ -1,3 +1,9 @@
+require "optparse"
+
+require "vagrant/hivemind/constants"
+require "vagrant/hivemind/util"
+require "vagrant/hivemind/host"
+
 module Vagrant
   module Hivemind
     module Command
@@ -7,7 +13,87 @@ module Vagrant
         end
 
         def execute
-          @env.ui.info "Morph!"
+          options = {
+            :hostname       => nil,
+            :ip_address     => nil,
+            :control        => nil,
+            :size           => nil,
+            :forwarded_port => nil,
+            :directory      => []
+          }
+
+          parser = OptionParser.new do |o|
+            o.banner = "Usage: vagrant hivemind morph [options]"
+            o.separator ""
+            o.separator "Options:"
+            o.separator ""
+
+            o.on("-n", "--hostname HOSTNAME", "The hostname of the Drone (REQUIRED)") do |n|
+              options[:hostname] = n
+            end
+
+            o.on("-a", "--ip-address IPADDRESS", "The new IP address of the Drone") do |a|
+              options[:ip_address] = a
+            end
+
+            o.on("-c", "--control", "Promote the Drone to be a Control Machine") do |c|
+              options[:control] = c
+            end
+
+            o.on("-s", "--size SIZE", Vagrant::Hivemind::Constants::BOX_SIZES.keys.map(&:to_s), "The new Box Size of the Drone [:extra_small, :small, :medium, :large, :extra_large] (default: :small)") do |s|
+              options[:size] = s
+            end
+
+            o.on("-p", "--forwarded-port PORTPAIR", "Forward GUESTPORT to HOSTPORT (the format is GUESTPORT:HOSTPORT)") do |p|
+              options[:forwarded_port] = p
+            end
+
+            o.on("-d", "--directory DIRECTORY", "Specify the directory where '#{Vagrant::Hivemind::Constants::HIVE_FILE}' is located (default: current directory)") do |d|
+              options[:directory] << d
+            end
+          end
+
+          parser.parse!
+
+          work_dir = options[:directory].empty? ? "." : options[:directory].first
+
+          if options[:hostname]
+            if Vagrant::Hivemind::Util::HiveFile.exist? work_dir
+              hosts = Vagrant::Hivemind::Util::HiveFile.read_from work_dir
+
+              if hosts.values.map(&:hostname).include? options[:hostname]
+                # 0. Validate inputs
+                if options[:ip_address] and Vagrant::Hivemind::Util::Network.is_valid_ip_address? options[:ip_address]
+                end
+
+                # TODO: Morph!
+                # 1. Get Drone
+                # 2. Modify Drone
+                # 3. Detach Drone from hosts
+                # 4. Re-attach morphed Drone
+
+                # 1.
+                drone = hosts[options[:hostname]]
+
+                # 2.
+                
+
+                # 3.
+                hosts.delete options[:hostname]
+                # 4.
+                Vagrant::Hivemind::Util::HiveFile.write_to hosts, work_dir
+                @env.ui.info "Morphed the Drone with hostname '#{options[:hostname]}'"
+              else
+                @env.ui.info "The specified hostname does not exist!"
+              end
+
+            else
+              @env.ui.info "There is no Hive file in the working directory."
+            end
+          else
+            parser.parse %w[--help]
+          end
+
           0
         end
       end
