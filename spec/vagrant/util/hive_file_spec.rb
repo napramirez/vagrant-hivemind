3e8c89f40a17e3dfb8c8f16798de0a78c415a09b
Nap Ramirez <napramirez@gmail.com>
2015-09-02 03:06:59 -0400

    Added tests for HiveFile#get_hive_file_from_path

--- spec/vagrant/util/hive_file_spec.rb
+++ spec/vagrant/util/hive_file_spec.rb
@@ -1,4 +1,5 @@
 require "spec_helper"
+require "vagrant/hivemind/constants"
 require "vagrant/hivemind/util"
 
 describe Vagrant::Hivemind::Util::HiveFile, "#exist?" do
@@ -23,4 +24,32 @@ describe Vagrant::Hivemind::Util::HiveFile, "#exist?" do
     end
   end
 
+end
+
+describe Vagrant::Hivemind::Util::HiveFile, "#get_hive_file_from_path" do
+
+  before :each do
+    Vagrant::Hivemind::Util::HiveFile.send(:public, *Vagrant::Hivemind::Util::HiveFile.private_instance_methods)
+  end
+
+  context "when there's a specified directory path" do
+    it "should return the current directory's hive.yml" do
+      Dir.mktmpdir "test_dir" do |test_dir|
+        specified_path = Pathname.new(test_dir)
+        expected_path = specified_path.join Vagrant::Hivemind::Constants::HIVE_FILE
+        result = Vagrant::Hivemind::Util::HiveFile.get_hive_file_from_path specified_path
+        expect(result.to_s).to eq expected_path.to_s
+      end
+    end
+  end
+
+  context "when there's a specified file path" do
+    it "should return the specified file path" do
+      Dir.mktmpdir "test_dir" do |test_dir|
+        specified_path = Pathname.new(test_dir).join "custom.hive.yml"
+        result = Vagrant::Hivemind::Util::HiveFile.get_hive_file_from_path specified_path
+        expect(result.to_s).to eq specified_path.to_s
+      end
+    end
+  end
 end
